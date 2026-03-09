1ad94774d30bfd79f3704d610ac84f3cedb79478
Kyuyeun Kim <kyuyeunk@google.com>
2026-03-09 05:51:11 +0000

    gmm down v1
    Signed-off-by: Kyuyeun Kim <kyuyeunk@google.com>

--- tpu_inference/layers/common/fused_moe_gmm.py
+++ tpu_inference/layers/common/fused_moe_gmm.py
@@ -63,7 +63,7 @@ def _swigluoai(x1: jax.Array,
 
 def gmm_wrapper(lhs, rhs, rhs_scale, rhs_bias, group_sizes, group_offset,
                 last_gmm):
-    if is_supported_by_gmm_v2(rhs_scale):
+    if is_supported_by_gmm_v2(rhs_scale) and not last_gmm:
         gmm_res = gmm_v2(
             lhs=lhs,
             rhs=rhs,
